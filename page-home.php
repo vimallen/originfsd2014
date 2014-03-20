c8c3c6ca238010ceb862ac147ba98046238ec096
Dave Allen <davidallen401@btinternet.com>
2014-03-20 21:04:08 +0000

    basic pelican working. Set body height to 100%

--- page-home.php
+++ page-home.php
@@ -11,21 +11,32 @@
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<div class="box1">
-	<div class="logo-home">
-<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/logo-fsd.png" alt="home page" /></a>
-</div><!--logo-home-->
-<?php wp_nav_menu( array( 'menu_class' => 'nav-home',) ); ?>
-</div><!--box1-->
+<div class="flexible-box" id="flexible-box_logo-and-sign-up">
+	<div id="logo-and-sign-up">
+    			
+<section id="logo">
+	<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/logo-fsd.png" alt="home page" /></a>
+</section> <!-- /logo -->
 
-<div class="box2">
+<section id="sign-up">
+<?php wp_nav_menu( array( 'menu_class' => 'nav-home',) ); ?>
+	</section> <!-- /sign-up -->	
+			
+    		</div> <!-- /logo-and-sign-up -->
+</div><!-- /flexible-box -->
+
+<div class="flexible-box" id="flexible-box_preorder">
+	<div id="preorder-box">
+		<section id="preorder">
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 <?php the_content(); ?>
 
 <?php endwhile; ?>
 
-
-</div>
+</section> <!-- /preorder -->
+				
+	</div> <!-- /preorder-box -->
+</div><!-- /flexible-box -->
 
 
