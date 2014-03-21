4534b785ca450f48d80925bc91048f9315600477
Dave Allen <davidallen401@btinternet.com>
2014-03-21 20:15:17 +0000

    new gitignore

--- page-home.php
+++ page-home.php
@@ -11,31 +11,32 @@
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<div class="flexible-box" id="flexible-box_logo">
+<div class="flexible-box flexible-box_logo">
 	<div id="logo">
-	 <section id="logo">
+    			
+<section id="logo">
 	<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/logo-fsd.png" alt="home page" /></a>
 </section> <!-- /logo -->
 
-<section id="nav">
+<nav>
 <?php wp_nav_menu( array( 'menu_class' => 'nav-home',) ); ?>
-	</section> <!-- /nav -->	
+	</nav> <!-- /nav -->	
 			
     		</div> <!-- /logo -->
 </div><!-- /flexible-box -->
 
-<div class="flexible-box" id="flexible-box_intro-home">
-	<div id="preorder-box">
-		<section id="preorder">
+<div class="flexible-box flexible-box_intro-home">
+	<div class="intro-box">
+		<section id="intro">
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 <?php the_content(); ?>
 
 <?php endwhile; ?>
 
-</section> <!-- /preorder -->
+</section> <!-- /intro -->
 				
-	</div> <!-- /preorder-box -->
+	</div> <!-- /intro-box -->
 </div><!-- /flexible-box -->
 
 
